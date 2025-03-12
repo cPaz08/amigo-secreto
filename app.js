4069fd1ddfab1045476ab6ebf7e2ecc464cd62fa
cPaz08 <pazvaldivia0807@gmail.com>
2025-03-12 18:26:09 -0500

    se ha añadido 2 funciones(agregarAmigo y actualizarAmigoLista)

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreDeAmigo = document.getElementById('amigo').value;
+    if (nombreDeAmigo == ''){
+        alert('Por favor, inserte un nombre');
+    }else {
+        amigos.push(nombreDeAmigo);
+        console.log(amigos);
+        document.getElementById('amigo').value = '';
+        actualizarAmigoLista();
+    }
+}
+
+function actualizarAmigoLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        let elemento = document.createElement('li');
+        elemento.textContent = amigos[i];
+        lista.appendChild(elemento);
+    }
+}
+
+function sortearAmigo()
